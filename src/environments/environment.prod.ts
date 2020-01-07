68c48ad34245eaf9819f21432495eed05f32fe5d
ShubhamMore <shubhammore1796@gmail.com>
2020-01-07 22:37:38 +0530

    dancing-soul

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   encKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
-  url: 'https://www.thedancingsoul.in/'
+  url: 'https://api.thedancingsoul.in/'
 };
